bb26aa9c2fc14c0370f1522e20a94ab1a97c8ff3
Adam Snetman <adam@startingnow.co>
2014-07-18 20:40:34 -0400

    Translation set up for global header and footer;

--- craft/translations/es.php
+++ craft/translations/es.php
@@ -2,5 +2,33 @@
 // In my templates, do this to translate static text:
 // <h1>{{ "News" | translate }}</h1>
 return array(
-    "News" => "Noticias",
+
+  // Main navigation
+  "Home" => "Inicio",
+  "Why Leatherbacks <br>&amp; Sea Turtles Matter" => "Porque Leatherbacks <br>&amp; Sea Turtles Matter",
+  "Threats" => "Amenazas",
+  "Conservation" => "Conservación",
+  "Donate" => "Done",
+
+  // Secondary nav
+  "About" => "About",
+  "News & Events" => "Noticias y Events",
+  "Contribute" => "Contribute",
+
+  // Footer nav
+  "Contact" => "Contacto",
+  "Site Terms" => "Site Terms",
+  "Privacy" => "Privacy",
+  "Back to top" => "a Top",
+
+  // Mobile nav, but hidden via css
+  "Menu" => "Menu",
+  "Close" => "Ciuda",
+
+  // Action links
+  "View all" => "a todo",
+  "More" => "Mas",
+  "Less" => "Less",
+  "Top" => "Top",
+
 );
